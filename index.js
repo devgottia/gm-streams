f01176d7f879297935c63247df88643aafc78789
devgottia <157880219+devgottia@users.noreply.github.com>
2024-04-10 21:55:59 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,68 @@
+$.ajax({
+  url: "https://sportea.eu/v1/api.php?key=a38088a3a9ba7c256f6580413927a274",
+  method: "GET",
+  dataType: "json",
+})
+  .done(function (data) {
+    const schedule = data[0].schedule;
+    const displayDiv = $("#display");
+
+    Object.keys(schedule).forEach((sport) => {
+      schedule[sport].forEach((event, index) => {
+        const eventTime = new Date(event.event_time * 1000);
+        const formattedTime = eventTime.toLocaleTimeString([], {
+          hour: "2-digit",
+          minute: "2-digit",
+          hour12: true,
+        });
+
+        displayDiv.append(`
+        <div class="col-md-4 mb-2">
+
+<div class="card mb-2 rounded-4 bg-body-tertiary">
+<div class="card-body text-center">
+
+<div >
+
+<span class="me-2">${data[0].date} </span>
+:
+&nbsp;
+<span class="me-2">${formattedTime} </span>
+<br />
+ 
+<span >${event.teams}  </span> 
+
+
+
+</div>
+
+
+<div>
+<button class="btn btn-outline-success copyBtn  w-100 mt-1 rounded-4" onclick="copyToClipboard('${event.channel_id}')" >Copy Link</button> 
+</div>
+
+</div>
+
+</div>
+
+        </div>
+        
+        `);
+      });
+    });
+  })
+  .fail(function () {
+    console.error("Request failed");
+  });
+
+function copyToClipboard(ch_Id) {
+  let copyUrl = encodeURI(
+    `https://stream.espnonline.site/live/embed.php?ch=${ch_Id}`
+  );
+
+  window.navigator.clipboard.writeText(copyUrl);
+}
+
+$("body").on("click", ".copyBtn", function () {
+  $(this).addClass("btn-outline-danger");
+});
