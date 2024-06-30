ebba8abaa0b8ef00a87ef177b056c9b8e453e5b7
devgottia <157880219+devgottia@users.noreply.github.com>
2024-06-30 21:06:38 +0500

    Update index.js

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ $.ajax({
 
 function copyToClipboard(ch_Id) {
   let copyUrl = encodeURI(
-    `https://v1.gmstreams.site/p/p.html?ch=${ch_Id}`
+    `https://www.coinsreport.xyz/p/p.html?ch=${ch_Id}`
   );
 
   window.navigator.clipboard.writeText(copyUrl);
